37acc6cc9b8b1f00f6bc633b3049742ef3ba5ecc
safieoscar <65041538+safieoscar@users.noreply.github.com>
2020-06-30 23:39:09 -0400

    added completed homework

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "cdcbb2e363417395481dfd1a6fcabfbb"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD9tCjzpg06-hZWErti-wqw6zAYY5wl8f8"
